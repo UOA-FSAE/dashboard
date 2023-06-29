08f8e954f12b69fa0e61419a2c919134fa3b7088
tazukiswift <tosb789@aucklanduni.ac.nz>
2023-06-29 15:23:56 +1200

    implemented base of can callbacks with correct ID

---
+++ Core/Inc/can_callbacks.h
@@ -0,0 +1,27 @@
+//
+// Created by tazukiswift on 29/06/23.
+//
+
+#ifndef DASHBOARD_CAN_CALLBACKS_H
+#define DASHBOARD_CAN_CALLBACKS_H
+
+#define updateCANFreq               500.0
+
+// CAN frame id
+#define CAN_ID_ACCUMULATOR_PACK_DATA       0x6B0
+#define CAN_ID_ACCUMULATOR_PACK_DATA_2     0x6B1
+#define CAN_ID_ACCUMULATOR_PACK_DATA_3     0x6B2
+#define CAN_ID_ACCUMULATOR_VOLTAGES        0x6B3
+#define CAN_ID_ACCUMULATOR_TEMPERATURES    0x6B4
+
+#define CAN_ID_GLV                         0x602
+#define CAN_ID_FAULT                       0x603
+#define CAN_ID_RACE_DATA                   0x604
+#define CAN_ID_DRIVER_DATA                 0x605
+
+#define CAN_ID_REAR_LEFT_DRIVE             0x384
+#define CAN_ID_FRONT_LEFT_DRIVE            0x385
+#define CAN_ID_FRONT_RIGHT_DRIVE           0x386
+#define CAN_ID_REAR_RIGHT_DRIVE            0x387
+
+#endif //DASHBOARD_CAN_CALLBACKS_H
