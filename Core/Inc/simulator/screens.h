c2d2aede6a5d8796ae9a7a81d0276808e6b40bff
tazukiswift <toby.osbornes.nz@gmail.com>
2023-11-03 13:30:24 +1300

    small updates to work with simulator

---
+++ Core/Inc/simulator/screens.h
@@ -0,0 +1,29 @@
+
+#ifndef INC_SCREENS_H_
+#define INC_SCREENS_H_
+
+// Comment Out if in actual dash
+//#define USE_SIMULATOR
+
+enum SCREENS {
+	DRIVER_SCREEN,
+	DEBUG_SCREEN,
+    ENERGY_SCREEN,
+    LAP_SCREEN
+};
+
+// CALL THIS FIRST
+void init_displays();
+
+// CALL THIS SECOND
+void init_screens();
+
+void try_update_screen();
+
+void update_screen();
+
+void try_cycle_screens();
+
+void cycle_screens();
+
+#endif /* INC_SCREENS_H_ */
